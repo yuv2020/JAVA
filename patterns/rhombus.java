48852e3d6448d756303bf001a40696e976449309
yuv2020 <yuvraj252122@gmail.com>
2024-09-11 10:58:52 +0530

    add files in rhombus

--- patterns/rhombus.java
+++ patterns/rhombus.java
@@ -20,7 +20,7 @@ public class rhombus {
         }
         
         // reverse the pattern for the second half
-        for (int i = n - 2; i >= 1; i--) {
+        for (int i = n; i >= 1; i--) {
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
