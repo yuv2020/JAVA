840846c34fdf3275f01060040c4c1f2392f11035
yuv2020 <yuvraj252122@gmail.com>
2025-01-02 14:11:51 +0530

    commits

--- array/frequency.java
+++ array/frequency.java
@@ -5,7 +5,27 @@ import java.util.*;
 // Find the count frequency of each element in an array.
 
 public class frequency {
+    public static void freq(int[] arr, int n){
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if(freqMap.containsKey(arr[i])){
+                freqMap.put(arr[i], freqMap.get(arr[i]) + 1);
+            }
+            else{
+                freqMap.put(arr[i], 1);
+            }
+        }
+        for(Map.Entry<Integer, Integer> entry : freqMap.entrySet()){    
+            System.out.println("Number: " + entry.getKey() + ", Frequency: " + entry.getValue());
+        }
+    }
+
+
     public static void main(String[] args) {
+        int arr[] = {10,56,32, 56, 2 , 12 , 2, 12 ,89, 63};
+        int n = arr.length;
+        freq(arr, n);
+
     
     }
     
---
+++ array/increase.java
@@ -0,0 +1,9 @@
+package array;
+
+public class increase {
+    public static void main(String[] args){
+        int[] arr = {1, 2, 3, 4, 5};
+        int[] newArr = new int[arr.length];
+    }
+    
+}
