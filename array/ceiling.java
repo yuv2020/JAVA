f6b26437b347eec67d52e73e44593d8e91936ae5
yuv2020 <yuvraj252122@gmail.com>
2025-01-13 20:25:12 +0530

    creating file

---
+++ array/ceiling.java
@@ -0,0 +1,32 @@
+
+// Ceiling of a number using the bst property in sorted array
+//Implement the first element to be searched first
+public class ceiling {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 5, 6};
+        int target = 5;
+        int index = binarySearch(arr, target);
+        System.out.println("The value of : " + index);
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[mid] > target){
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return arr[left]; // return the index of the first element greater than or equal to target
+    }    
+}
+
+
